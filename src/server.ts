7476c40cc71a269c2cf8c4e2d023293fb3aef34a
Jo-x <vasilij.alexsandrovich@gmail.com>
2022-11-22 19:35:36 +0600

    Added db MYSQL

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.use(cors({
+	origin: '*'
+}));
+
+app.get('/api/users', (req: Request, res: Response) => {
+	res.send(
+		require('../data/users/users.json')
+	);
 });
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+	console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
