3e85e79060add26f896c4adcd8e3928dba059782
Jo-x <vasilij.alexsandrovich@gmail.com>
2022-11-29 00:22:25 +0600

    test done

--- dist/server.js
+++ dist/server.js
@@ -1,112 +1,112 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mysql2_1 = __importDefault(require("mysql2"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+const bodyParser = __importStar(require("body-parser"));
+const Utils_1 = require("./utils/Utils");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
-app.get('/', (_req, res) => {
-    return res.send('Express Typescript on Vercel');
+const urlencodedParser = express_1.default.urlencoded({ extended: false });
+const pool = mysql2_1.default.createPool({
+    connectionLimit: 5,
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'myApp',
+}).promise();
+app.use((0, cors_1.default)({ origin: '*' }));
+app.use(bodyParser.json());
+app.get('/api/users', (req, res) => {
+    pool.query(`SELECT * FROM Users`)
+        .then(r => res.send(r[0]))
+        .catch(e => console.log(e.message));
 });
-app.get('/ping', (_req, res) => {
-    return res.send('pong 🏓');
+app.post('/block-users', urlencodedParser, (req, res) => {
+    const usersId = req.body.usersId;
+    if (!req.body)
+        return res.sendStatus(400);
+    if (usersId.length > 0) {
+        pool.query(`UPDATE Users SET isBlocked=1 WHERE id in (${usersId.join()})`)
+            .then(() => pool.query(`SELECT * FROM Users`))
+            .then(r => res.send(r))
+            .catch(e => console.log(e.message));
+    }
+});
+app.post('/unblock-users', urlencodedParser, (req, res) => {
+    const usersId = req.body.usersId;
+    if (!req.body)
+        return res.sendStatus(400);
+    if (usersId.length > 0) {
+        pool.query(`UPDATE Users SET isBlocked=0 WHERE id in (${usersId.join()})`)
+            .then(() => pool.query(`SELECT * FROM Users`))
+            .then(r => res.send(r))
+            .catch(e => console.log(e.message));
+    }
+});
+app.delete('/delete-users', urlencodedParser, (req, res) => {
+    const usersId = req.body;
+    if (!req.body)
+        return res.sendStatus(400);
+    if (usersId.length > 0) {
+        pool.query(`DELETE FROM Users WHERE id in (${usersId.join()})`)
+            .then(() => pool.query(`SELECT * FROM Users`))
+            .then(r => res.send(r))
+            .catch(e => console.log(e.message));
+    }
+});
+app.post('/registration', urlencodedParser, (req, res) => {
+    const userData = req.body;
+    const today = new Date();
+    if (!userData)
+        return res.sendStatus(400);
+    pool.query(`INSERT Users(firstName, lastName, lastLogin, registrationDate, isBlocked, email, hash)
+		VALUES ( 
+			"${userData.firstName}",
+			"${userData.lastName}",
+			"${(0, Utils_1.getNowDate)(today, true)}",
+			"${(0, Utils_1.getNowDate)(today, false)}",
+			FALSE,
+			"${userData.email}",
+			"${(0, Utils_1.getHash)(userData.password, 256)}");`).then(() => pool.query(`SELECT IF (EXISTS (SELECT email FROM Users WHERE email="${userData.email}"), 1,0) AS isSucceeded;`).then(r => res.send(r[0][0]))).catch(e => console.log(e.message));
+});
+app.post('/authentication', urlencodedParser, (req, res) => {
+    const userData = req.body;
+    if (!userData)
+        return res.sendStatus(400);
+    pool.query(`SELECT IF (EXISTS (SELECT email, hash, isBlocked FROM Users
+			WHERE email="${userData.email}" AND hash="${(0, Utils_1.getHash)(userData.password, 256)}" AND isBlocked=0), 1,0) AS isSucceeded;`).then(r => res.send(r[0][0]));
 });
 app.listen(port, () => {
-    return console.log(`Server is listening on ${port}`);
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
-// import express, { Express, Request, Response } from 'express';
-// import mysql, { RowDataPacket } from 'mysql2'
-// import dotenv from 'dotenv';
-// import cors from 'cors';
-// import * as bodyParser from 'body-parser';
-// import { BodyAnswer, IAuthenticationData, IRegistrationData, IRequestUserId, isSucceeded, ITypedRequestBody } from 'interfaces/UserInterface';
-// import { getHash, getNowDate } from './utils/Utils'
-// dotenv.config();
-// const app: Express = express();
-// const port = process.env.PORT;
-// const urlencodedParser = express.urlencoded({ extended: false });
-// const pool = mysql.createPool({
-// 	connectionLimit: 5,
-// 	host: 'localhost',
-// 	port: 3306,
-// 	user: 'root',
-// 	password: 'root',
-// 	database: 'myApp',
-// }).promise();
-// app.use(cors({ origin: '*' }));
-// app.use(bodyParser.json());
-// app.get('/api/users', (req: Request, res: Response) => {
-// 	pool.query(`SELECT * FROM Users`)
-// 		.then(r => res.send(r[0]))
-// 		.catch(e => console.log(e.message));
-// });
-// app.post('/block-users', urlencodedParser, (req: ITypedRequestBody<IRequestUserId>, res: Response) => {
-// 	const usersId = req.body.usersId;
-// 	if (!req.body)
-// 		return res.sendStatus(400);
-// 	if (usersId.length > 0) {
-// 		pool.query(`UPDATE Users SET isBlocked=1 WHERE id in (${usersId.join()})`)
-// 			.then(() => pool.query(`SELECT * FROM Users`))
-// 			.then(r => res.send(r))
-// 			.catch(e => console.log(e.message))
-// 	}
-// });
-// app.post('/unblock-users', urlencodedParser, (req: ITypedRequestBody<IRequestUserId>, res: Response) => {
-// 	const usersId = req.body.usersId;
-// 	if (!req.body)
-// 		return res.sendStatus(400);
-// 	if (usersId.length > 0) {
-// 		pool.query(`UPDATE Users SET isBlocked=0 WHERE id in (${usersId.join()})`)
-// 			.then(() => pool.query(`SELECT * FROM Users`))
-// 			.then(r => res.send(r))
-// 			.catch(e => console.log(e.message))
-// 	}
-// });
-// app.delete('/delete-users', urlencodedParser, (req: ITypedRequestBody<number[]>, res: Response) => {
-// 	const usersId = req.body;
-// 	if (!req.body)
-// 		return res.sendStatus(400);
-// 	if (usersId.length > 0) {
-// 		pool.query(`DELETE FROM Users WHERE id in (${usersId.join()})`)
-// 			.then(() => pool.query(`SELECT * FROM Users`))
-// 			.then(r => res.send(r))
-// 			.catch(e => console.log(e.message))
-// 	}
-// });
-// app.post('/registration', urlencodedParser, (req: ITypedRequestBody<IRegistrationData>, res: Response) => {
-// 	const userData = req.body;
-// 	const today = new Date();
-// 	if (!userData)
-// 		return res.sendStatus(400);
-// 	pool.query(
-// 		`INSERT Users(firstName, lastName, lastLogin, registrationDate, isBlocked, email, hash)
-// 		VALUES ( 
-// 			"${userData.firstName}",
-// 			"${userData.lastName}",
-// 			"${getNowDate(today, true)}",
-// 			"${getNowDate(today, false)}",
-// 			FALSE,
-// 			"${userData.email}",
-// 			"${getHash(userData.password, 256)}");`
-// 	).then(
-// 		() => pool.query<isSucceeded>(
-// 			`SELECT IF (EXISTS (SELECT email FROM Users WHERE email="${userData.email}"), 1,0) AS isSucceeded;`
-// 		).then(r => res.send(r[0][0]))
-// 	).catch(e => console.log(e.message))
-// });
-// app.post('/authentication', urlencodedParser, (req: ITypedRequestBody<IAuthenticationData>, res: Response) => {
-// 	const userData = req.body;
-// 	if (!userData)
-// 		return res.sendStatus(400);
-// 	pool.query<isSucceeded>(
-// 		`SELECT IF (EXISTS (SELECT email, hash, isBlocked FROM Users
-// 			WHERE email="${userData.email}" AND hash="${getHash(userData.password, 256)}" AND isBlocked=0), 1,0) AS isSucceeded;`
-// 	).then(r => res.send(r[0][0]))
-// });
-// app.listen(port, () => {
-// 	console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-// });
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,125 +1,105 @@
-import express, { Request, Response } from 'express'
+import express, { Express, Request, Response } from 'express';
+import mysql from 'mysql2'
 import dotenv from 'dotenv';
+import cors from 'cors';
+import * as bodyParser from 'body-parser';
+import { IAuthenticationData, IRegistrationData, IRequestUserId, isSucceeded, ITypedRequestBody } from 'interfaces/UserInterface';
+import { getHash, getNowDate } from './utils/Utils'
 
 dotenv.config();
-const app = express()
+
+const app: Express = express();
 const port = process.env.PORT;
+const urlencodedParser = express.urlencoded({ extended: false });
 
-app.get('/', (_req: Request, res: Response) => {
-  return res.send('Express Typescript on Vercel')
-})
+const pool = mysql.createPool({
+	connectionLimit: 5,
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: 'root',
+	database: 'myApp',
+}).promise();
 
-app.get('/ping', (_req: Request, res: Response) => {
-  return res.send('pong 🏓')
-})
+app.use(cors({ origin: '*' }));
+app.use(bodyParser.json());
+
+app.get('/api/users', (req: Request, res: Response) => {
+	pool.query(`SELECT * FROM Users`)
+		.then(r => res.send(r[0]))
+		.catch(e => console.log(e.message));
+});
+
+app.post('/block-users', urlencodedParser, (req: ITypedRequestBody<IRequestUserId>, res: Response) => {
+	const usersId = req.body.usersId;
+	if (!req.body)
+		return res.sendStatus(400);
+	if (usersId.length > 0) {
+		pool.query(`UPDATE Users SET isBlocked=1 WHERE id in (${usersId.join()})`)
+			.then(() => pool.query(`SELECT * FROM Users`))
+			.then(r => res.send(r))
+			.catch(e => console.log(e.message))
+	}
+});
+
+app.post('/unblock-users', urlencodedParser, (req: ITypedRequestBody<IRequestUserId>, res: Response) => {
+	const usersId = req.body.usersId;
+	if (!req.body)
+		return res.sendStatus(400);
+	if (usersId.length > 0) {
+		pool.query(`UPDATE Users SET isBlocked=0 WHERE id in (${usersId.join()})`)
+			.then(() => pool.query(`SELECT * FROM Users`))
+			.then(r => res.send(r))
+			.catch(e => console.log(e.message))
+	}
+});
+
+app.delete('/delete-users', urlencodedParser, (req: ITypedRequestBody<number[]>, res: Response) => {
+	const usersId = req.body;
+	if (!req.body)
+		return res.sendStatus(400);
+	if (usersId.length > 0) {
+		pool.query(`DELETE FROM Users WHERE id in (${usersId.join()})`)
+			.then(() => pool.query(`SELECT * FROM Users`))
+			.then(r => res.send(r))
+			.catch(e => console.log(e.message))
+	}
+});
+
+app.post('/registration', urlencodedParser, (req: ITypedRequestBody<IRegistrationData>, res: Response) => {
+	const userData = req.body;
+	const today = new Date();
+	if (!userData)
+		return res.sendStatus(400);
+
+	pool.query(
+		`INSERT Users(firstName, lastName, lastLogin, registrationDate, isBlocked, email, hash)
+		VALUES ( 
+			"${userData.firstName}",
+			"${userData.lastName}",
+			"${getNowDate(today, true)}",
+			"${getNowDate(today, false)}",
+			FALSE,
+			"${userData.email}",
+			"${getHash(userData.password, 256)}");`
+	).then(
+		() => pool.query<isSucceeded>(
+			`SELECT IF (EXISTS (SELECT email FROM Users WHERE email="${userData.email}"), 1,0) AS isSucceeded;`
+		).then(r => res.send(r[0][0]))
+	).catch(e => console.log(e.message))
+});
+
+app.post('/authentication', urlencodedParser, (req: ITypedRequestBody<IAuthenticationData>, res: Response) => {
+	const userData = req.body;
+	if (!userData)
+		return res.sendStatus(400);
+
+	pool.query<isSucceeded>(
+		`SELECT IF (EXISTS (SELECT email, hash, isBlocked FROM Users
+			WHERE email="${userData.email}" AND hash="${getHash(userData.password, 256)}" AND isBlocked=0), 1,0) AS isSucceeded;`
+	).then(r => res.send(r[0][0]))
+});
 
 app.listen(port, () => {
-  return console.log(`Server is listening on ${port}`)
-})
-
-
-// import express, { Express, Request, Response } from 'express';
-// import mysql, { RowDataPacket } from 'mysql2'
-// import dotenv from 'dotenv';
-// import cors from 'cors';
-// import * as bodyParser from 'body-parser';
-// import { BodyAnswer, IAuthenticationData, IRegistrationData, IRequestUserId, isSucceeded, ITypedRequestBody } from 'interfaces/UserInterface';
-// import { getHash, getNowDate } from './utils/Utils'
-
-// dotenv.config();
-
-// const app: Express = express();
-// const port = process.env.PORT;
-// const urlencodedParser = express.urlencoded({ extended: false });
-
-// const pool = mysql.createPool({
-// 	connectionLimit: 5,
-// 	host: 'localhost',
-// 	port: 3306,
-// 	user: 'root',
-// 	password: 'root',
-// 	database: 'myApp',
-// }).promise();
-
-// app.use(cors({ origin: '*' }));
-// app.use(bodyParser.json());
-
-// app.get('/api/users', (req: Request, res: Response) => {
-// 	pool.query(`SELECT * FROM Users`)
-// 		.then(r => res.send(r[0]))
-// 		.catch(e => console.log(e.message));
-// });
-
-// app.post('/block-users', urlencodedParser, (req: ITypedRequestBody<IRequestUserId>, res: Response) => {
-// 	const usersId = req.body.usersId;
-// 	if (!req.body)
-// 		return res.sendStatus(400);
-// 	if (usersId.length > 0) {
-// 		pool.query(`UPDATE Users SET isBlocked=1 WHERE id in (${usersId.join()})`)
-// 			.then(() => pool.query(`SELECT * FROM Users`))
-// 			.then(r => res.send(r))
-// 			.catch(e => console.log(e.message))
-// 	}
-// });
-
-// app.post('/unblock-users', urlencodedParser, (req: ITypedRequestBody<IRequestUserId>, res: Response) => {
-// 	const usersId = req.body.usersId;
-// 	if (!req.body)
-// 		return res.sendStatus(400);
-// 	if (usersId.length > 0) {
-// 		pool.query(`UPDATE Users SET isBlocked=0 WHERE id in (${usersId.join()})`)
-// 			.then(() => pool.query(`SELECT * FROM Users`))
-// 			.then(r => res.send(r))
-// 			.catch(e => console.log(e.message))
-// 	}
-// });
-
-// app.delete('/delete-users', urlencodedParser, (req: ITypedRequestBody<number[]>, res: Response) => {
-// 	const usersId = req.body;
-// 	if (!req.body)
-// 		return res.sendStatus(400);
-// 	if (usersId.length > 0) {
-// 		pool.query(`DELETE FROM Users WHERE id in (${usersId.join()})`)
-// 			.then(() => pool.query(`SELECT * FROM Users`))
-// 			.then(r => res.send(r))
-// 			.catch(e => console.log(e.message))
-// 	}
-// });
-
-// app.post('/registration', urlencodedParser, (req: ITypedRequestBody<IRegistrationData>, res: Response) => {
-// 	const userData = req.body;
-// 	const today = new Date();
-// 	if (!userData)
-// 		return res.sendStatus(400);
-
-// 	pool.query(
-// 		`INSERT Users(firstName, lastName, lastLogin, registrationDate, isBlocked, email, hash)
-// 		VALUES ( 
-// 			"${userData.firstName}",
-// 			"${userData.lastName}",
-// 			"${getNowDate(today, true)}",
-// 			"${getNowDate(today, false)}",
-// 			FALSE,
-// 			"${userData.email}",
-// 			"${getHash(userData.password, 256)}");`
-// 	).then(
-// 		() => pool.query<isSucceeded>(
-// 			`SELECT IF (EXISTS (SELECT email FROM Users WHERE email="${userData.email}"), 1,0) AS isSucceeded;`
-// 		).then(r => res.send(r[0][0]))
-// 	).catch(e => console.log(e.message))
-// });
-
-// app.post('/authentication', urlencodedParser, (req: ITypedRequestBody<IAuthenticationData>, res: Response) => {
-// 	const userData = req.body;
-// 	if (!userData)
-// 		return res.sendStatus(400);
-
-// 	pool.query<isSucceeded>(
-// 		`SELECT IF (EXISTS (SELECT email, hash, isBlocked FROM Users
-// 			WHERE email="${userData.email}" AND hash="${getHash(userData.password, 256)}" AND isBlocked=0), 1,0) AS isSucceeded;`
-// 	).then(r => res.send(r[0][0]))
-// });
-
-// app.listen(port, () => {
-// 	console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-// });
+	console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
