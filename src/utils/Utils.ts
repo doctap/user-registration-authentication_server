249eb53461653cf203c4e12783d9a68fed7797ff
Jo-x <vasilij.alexsandrovich@gmail.com>
2022-11-30 01:09:26 +0600

    set tsconfig

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -2,7 +2,9 @@ import { SHA3 } from "sha3";
 
 type hashSizes = 512 | 224 | 256 | 384;
 
-export const getNowDate = (date: Date, time: boolean) =>
+const getNowDate = (date: Date, time: boolean) =>
 	`${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}${time ? ' ' + date.toLocaleTimeString() : ''}`;
 
-export const getHash = (a: string, size: hashSizes) => new SHA3(size).update(a).digest('hex');
+const getHash = (a: string, size: hashSizes) => new SHA3(size).update(a).digest('hex');
+
+export { getNowDate, getHash };
